5291b906e82cfe1703005c3a9c6b60475dfaae87
Niklas Hellmer <hellmer.niklas@t-online.de>
2023-04-24 11:33:01 +0200

    finishing touches

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Bearing fault detection with topological data analysis
+
+###Author: Niklas Hellmer, github.com/nihell, nhellmer at impan dot pl
+
 This repo accompanies our recent paper LINK.
-The monte-carlo simulations with synthatic data can be reproduced using the run_simultaions script.
-The idler_data and vibstand_data provide the analysis of data from the lab and the actual mine, respectively.
+
+The monte-carlo simulations with synthatic data can be reproduced using the run_simulations script. The code for generating the time series is symulacja_py.py.
+
+The idler_data and vibstand_data notebooks provide the analysis of data from the lab and the actual mine, respectively.
+
+The technical part of the code is contained in TDASignaltools.py.
+
+All the resulting figures shown in the paper are stored in the results folder.
+
 Data files are available upon request.
 
 ## Citing
---
+++ run_simulations.py
@@ -0,0 +1,29 @@
+from TDASignaltools import *
+
+### Main script to reproduce simulations on synthetic data as displayed in the paper
+
+alphas = np.linspace(1.6,2,9)
+amplitudes = np.linspace(5,9,9)
+
+mc_iterations = 1000
+
+functions = [betticurve, cvb_selector, infogram,infogram]
+args = [{"dim":2499, "delay":1, "skip":1}, {}, {"gn":True}, {"gn":False}]
+names = ["TDA", "CVB", "GNInfogram", "Infogram"]
+
+create_data=True
+
+for f, a, n  in zip(functions,args,names):
+    print(n)
+    print("running {} mc simulations".format(mc_iterations))
+    test_powers = compute_test_powers(alphas, amplitudes, mc_iterations, f, create_data=create_data, metric = "l1", **a)
+    create_data = False
+    #print(test_powers_alpha.shape)
+    np.savetxt("{}iterations_l1-metric_{}_test_powers.txt".format(mc_iterations, n),np.array(test_powers))
+
+    f,ax = plt.subplots(figsize = (len(amplitudes),len(alphas)))
+    plot_test_power_matrix(alphas, amplitudes, mc_iterations, test_powers, n, ax=ax)
+    plt.savefig("{}-powers{}.pdf".format(n,mc_iterations))
+    plt.show()
+    plt.clf()
+    plt.close()
